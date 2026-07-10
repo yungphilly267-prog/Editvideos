a93945174061354b443865f0aa64cdc91abe38ed
AppScout Build Agent <build@appscout.app>
2026-07-10 02:27:12 +0000

    fix: partial native build repairs

---
+++ ffmpeg_helper/lib/ffmpeg_helper.dart
@@ -0,0 +1,24 @@
+library ffmpeg_helper;
+
+export 'package:ffmpeg_kit_flutter_min_gpl/ffmpeg_kit.dart';
+export 'package:ffmpeg_kit_flutter_min_gpl/ffmpeg_session.dart';
+export 'package:ffmpeg_kit_flutter_min_gpl/ffprobe_kit.dart';
+export 'package:ffmpeg_kit_flutter_min_gpl/log.dart';
+export 'package:ffmpeg_kit_flutter_min_gpl/media_information.dart';
+export 'package:ffmpeg_kit_flutter_min_gpl/media_information_session.dart';
+export 'package:ffmpeg_kit_flutter_min_gpl/return_code.dart';
+export 'package:ffmpeg_kit_flutter_min_gpl/statistics.dart';
+export 'package:ffmpeg_kit_flutter_min_gpl/stream_information.dart';
+
+// Abstract
+export 'abstract_classes/exporter.dart';
+// Extensions
+export 'extensions/exporter.dart';
+// Args
+export 'ffmpeg/args/exporter.dart';
+// FFMPEG
+export 'ffmpeg/exporter.dart';
+// FFMPEG filters
+export 'ffmpeg/filters/exporter.dart';
+// Helpers
+export 'helpers/exporter.dart';
